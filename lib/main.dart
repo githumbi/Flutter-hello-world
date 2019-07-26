839768cd41a9769de3ef0b7f48b0cc7ded25edc0
githumbi <githumbi74@gmail.com>
2019-07-26 13:16:02 +0300

    generate random words

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
   Widget build(BuildContext context){
+    final wordPair = WordPair.random();
     return MaterialApp(
       title: 'welcome to fluuter',
       home: Scaffold(  //provides default appbar
         appBar: AppBar(
-          title: const Text('my first app'),
+          title: Text('my first app'),
         ),
-        body: const Center(
-          child: const Text('hello there'),
+        body: Center(
+          // child: const Text('hello there'),
+          child: Text(wordPair.asPascalCase), //generates random words from english_words library
         ),
       ),
     );
