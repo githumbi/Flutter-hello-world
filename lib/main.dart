ecb420f77c3c362a9c9126a5d0ec3b0b077f1cc0
githumbi <githumbi74@gmail.com>
2019-07-26 16:32:36 +0300

    added scroll activity

--- lib/main.dart
+++ lib/main.dart
@@ -4,32 +4,59 @@ import 'package:english_words/english_words.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    // final wordPair = WordPair.random();
+ @override
+  Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'welcome to fluuter',
-      home: Scaffold(  //provides default appbar
-        appBar: AppBar(
-          title: Text('my first app'),
-        ),
-        body: Center(
-          // child: const Text('hello there'),
-          // child: Text(wordPair.asPascalCase), //generates random words from english_words library
-          child: RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: RandomWords(),
     );
   }
 
 }
 
+// Stateful widgets maintain state that might change during the lifetime of the widget. Implementing a stateful widget requires at least two classes:
+//  1) a StatefulWidget class that creates an instance of 
+//  2) a State class. The StatefulWidget class is, itself, immutable, but the State class persists over the lifetime of the widget.
+
 class RandomWordsState extends State<RandomWords>{
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18);
+
+  Widget _buildSuggestions(){
+    return ListView.builder(
+      padding: const EdgeInsets.all(16),
+      itemBuilder: (BuildContext _context, int i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+
+        final int index = i ~/ 2;
+
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      }
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return ListTile(
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context){
-    final WordPair wordPair = WordPair.random();
-    return Text(wordPair.asPascalCase);
-  }
+    return Scaffold (                   // Add from here... 
+      appBar: AppBar(
+        title: Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    ); 
+}
 }
 
 class RandomWords extends StatefulWidget {
